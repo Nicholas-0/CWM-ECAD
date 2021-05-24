8d13a2426683a7542275ecd76b388d63814b3e2b
Cloud User <centos@ip-172-31-42-199.eu-west-2.compute.internal>
2021-05-24 15:50:17 +0000

    Made changes to Ex2 top.v and top_tb.v

--- Ex3/top.v
+++ Ex3/top.v
@@ -19,12 +19,33 @@
 `timescale 1ns / 100ps
 
 module monitor (
-    //Todo: add ports 
+    //Todo: add ports
+	input rst,
+	input change,
+	input on_off,
+	input clk,
+	output[7:0] counter_out
 
     );
                     
     //Todo: add registers and wires, if needed
+	reg counter_out;
 
     //Todo: add user logic
+	// use non blocking assignments
+	always @ (posedge clk) begin
+		// if rst == 1, counter_out is reset to 0
+		if (rst) counter_out <= 0;
+		else begin
+			// if change is 0, counter value stays constant
+			if (change == 0)  counter_out <= counter_out;
+			else
+				// if on_off is 1, increment counter_out
+				// if on_off is 0, decrement counter_out
+				if (on_off) counter_out = counter_out + 1;
+				else counter_out = counter_out - 1;
+		end
+	end
+			
       
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -12,17 +12,63 @@
 
 module top_tb(
     );
+
+
     
 //Todo: Parameters
+	parameter CLK_PERIOD = 10;
 
 //Todo: Regitsers and wires
+	reg clk;
+	reg rst;
+	reg change;
+	reg on_off;
+	reg err;
+	reg prev_counter_out;
+	wire counter_out;
 
 //Todo: Clock generation
+	initial begin
+		clk = 1'b0;
+		forever
+			#(CLK_PERIOD/2) clk=~clk;
+	end
 
 //Todo: User logic
+	rst = 1;
+	change = 0;
+	on_off = 1;
+	#(CLK_PERIOD*10)
+	if (counter_out != 0) begin
+		$display("***TEST FAILED! rst=%d, counter_out=%d", rst, counter_out);
+		err = 1;
+	end
+	#(CLK_PERIOD*10)
+	rst = 0;
+	prev_counter_out = counter_out;
+	#(CLK_PERIOD*10)
+	change = 1;
+	forever begin
+		#(CLK_PERIOD*10)
+		if (counter_out == prev_counter_out) begin
+			%display("***TEST FAILED!");
+			err = 1;
+		end
+		
+
+	
+
+		
     
 //Todo: Finish test, check for success
 
 //Todo: Instantiate counter module
+	monitor top (
+		.rst(rst),
+		.change(change),
+		.on_off(on_off),
+		.clk(clk),
+		.counter_out(counter_out)
+		);
  
 endmodule 
