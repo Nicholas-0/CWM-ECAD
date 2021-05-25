c359b24708ae53ed841658d37980d8043651a3f7
Cloud User <centos@ip-172-31-42-199.eu-west-2.compute.internal>
2021-05-25 11:59:41 +0000

    Made changes to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,35 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/ 100ps
+
+module LED(
+	input clk,
+	input rst,
+	input button,
+	output reg [2:0] colour
+	);
+
+	always @(posedge clk or button or posedge rst) begin
+		if (rst)
+			colour <= 3'b000;
+		else begin
+			if (colour == 3'b000 || 3'b111)
+				colour <= 3'b001;
+			else begin
+			if (colour == 3'b001 && button)
+				colour <= 3'b010;
+			if (colour == 3'b010 && button)
+				colour <= 3'b011;
+			if (colour == 3'b011 && button)
+				colour <= 3'b100;
+			if (colour == 3'b100 && button)
+				colour <= 3'b101;
+			if (colour == 3'b101 && button)
+				colour <= 3'b110;
+			if (colour == 3'b110 && button)
+				colour <= 3'b001;
+			end
+		end
+	end
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,117 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/ 100ps
+
+module top_tb(
+	);
+
+	parameter CLK_PERIOD = 6;
+	
+	reg clk;
+	reg rst;
+	reg button;
+	reg err;
+	wire [2:0] colour;
+
+	//Clock generation
+	initial begin
+		clk = 1'b0;
+		forever
+			#(CLK_PERIOD/2) clk=~clk;
+	end
+
+	//Stimulus logic
+	initial begin
+	rst = 0;
+	button = 0;
+	err = 0;
+	
+		#(CLK_PERIOD*2)
+		rst = 1;
+		#(CLK_PERIOD)
+		if (colour != 3'b000) begin
+			$display("***TEST FAILED due to RESET***");
+			err = 1;
+		end
+		forever begin
+		#(CLK_PERIOD)
+		rst = 0;
+		#(CLK_PERIOD*2)
+
+		if (colour != 3'b001) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+		
+		button = 1;
+		#(CLK_PERIOD)
+
+		if (colour != 3'b010) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		#(CLK_PERIOD)
+
+		if (colour != 3'b011) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		#(CLK_PERIOD)
+
+		if (colour != 3'b100) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		#(CLK_PERIOD)
+
+		if (colour != 3'b101) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		#(CLK_PERIOD)
+
+		if (colour != 3'b110) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		#(CLK_PERIOD)
+
+		if (colour != 3'b001) begin
+			$display("***TEST FAILED***");
+			err = 1;
+		end
+
+		
+		end
+	end
+
+	initial begin
+	#500
+	if (err==0)
+		$display("***TEST PASSED***");
+		$finish;
+	end
+
+	// Instantiate testing module
+	LED top(
+		.clk(clk),
+		.rst(rst),
+		.button(button),
+		.colour(colour)
+		);
+
+endmodule
+
+		
+		
+
+
+
+
+
