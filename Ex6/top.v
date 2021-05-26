c664e7877fd219d87aadf57a2a671db7ad598bf1
Cloud User <centos@ip-172-31-42-199.eu-west-2.compute.internal>
2021-05-26 11:51:15 +0000

    Made changes to top.v

--- Ex6/top.v
+++ Ex6/top.v
@@ -14,4 +14,37 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+
+
+module RGB_light(
+	input clk,
+	input [2:0] colour,
+	input enable,
+	output reg [23:0] rgb
+	);
+
+mybram your_instance_name (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea set to 0
+  .addra(addra),  // input wire [3 : 0] addra your colour
+  .dina(dina),    // input wire [15 : 0] dina set to 0
+  .douta(douta)  // output wire [15 : 0] douta
+);
+	assign clka = clk;
+	assign ena = enable;
+	assign wea = 0;
+	assign dina = 0;
+
+	
+	always @(posedge clka) begin
+		if ena begin
+			addra = colour;
+			rgb = douta;
+
+	end
+
+endmodule
 
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -6,4 +6,32 @@
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+	
+	parameter CLK_PERIOD = 6;
+	
+	reg clk;
+	reg [2:0] colour;
+	reg enable;
+	wire [23:0] rgb;
+	
+	// Clock generation
+	initial begin
+	clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+	// Stimulus logic
+	initial begin
+	
+
+
+	end
+
+endmodule
+
+	
